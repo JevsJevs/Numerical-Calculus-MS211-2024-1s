9cb1c03a9ad5c97bbd9256fd686bdaeb69e8c05f
Lucas Bellusci D'Andréa <l206009@dac.unicamp.br>
2024-06-05 17:33:25 -0300

    intervalo corrigido

--- Project2/main.py
+++ Project2/main.py
@@ -2,13 +2,13 @@ import math
 import euller as method1
 import rungeKutta4 as method2
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 #parameters:
 a, b = [0, 10]
 r = 0.5
 k = 10
-h = 5
+h = 0.05
 y0 = 1
 Euller = math.e
 
@@ -20,27 +20,22 @@ def func(t, y):
 def func_analitic(t):
     return k*y0*Euller**(r*t)/(k+y0*(Euller**(r*t)-1))
 
-def list_analitic():
-    list = []
-    n = int((b-a)/h)
-    for i in range(n+1):
-        t = a+i*h
-        list.append((t, func_analitic(t)))
-    return list
-
 #run methods
 listEuller = method1.euller(a, b, h, y0, func)
 listRK = method2.rungeKutta4(a, b, h, y0, func)
-listAnalitic = list_analitic()
 
+#last values
 print(listEuller[-1][-1])
 print(listRK[-1][-1])
-print(listAnalitic[-1][-1])
+print(func_analitic(b))
+
+#time
+t = np.linspace(a, b, 100)
 
 #plot
 plt.plot(*zip(*listEuller), label='Euller', color='red')
-plt.plot(*zip(*listRK), label='Runge-Kutta 4', color = 'green')
-plt.plot(*zip(*listAnalitic), label='Analitic', color = 'blue')
+plt.plot(*zip(*listRK), label='Runge-Kutta4', color = 'green')
+plt.plot(t, func_analitic(t), label='Analitic', color='blue')
 plt.show()
 
 
